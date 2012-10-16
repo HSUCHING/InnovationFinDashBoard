8c38d637d022b6feaf13e15fdf57a313529ef7d0
Hsuching <martin.hsuching@gmail.com>
2012-10-16 21:43:46 +0800

    update

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,5 +1,5 @@
 Ext.define("InnovationFinDashBoard.view.Main", {
-    extend: 'Ext.Panel',
+    extend: 'Ext.NavigationView',
 
     requires: [
         'Ext.Img'
@@ -9,37 +9,42 @@ Ext.define("InnovationFinDashBoard.view.Main", {
     config:{
         fullscreen:true,
         id:'myPanel',
-        layout:{
-            type:'vbox',
-            align:'center',
-            pack:'justify'
-        },
-        defaults:{
-            width:200,
-            height:200,
-            mode:'img',
-            listeners:{
-                tap:function(img,ev){
-                    Ext.Msg.alert(img.id);
 
-                }
-            }
-        },
         items:[{
-            xtype:'image',
-            id:'dupont',
-            src:'resources/images/dupont.jpg',
-            flex: 1
-
-        },{
-            xtype:'image',
-            src:'resources/images/Data_analysis.jpg',
-            flex: 1
-        },{
-            xtype:'image',
-            src:'resources/images/IDEA.jpg',
-            flex: 1
+            layout:{
+                type:'vbox',
+                align:'center',
+                pack:'justify',
+                fullscreen:true
+            },
+            defaults:{
+                width:200,
+                height:200,
+                mode:'img',
+                listeners:{
+                    tap:function(img,ev){
+                        Ext.Msg.alert(img.id);
+                        Ext.getCmp('myPanel').push({
+                            title:'Second',
+                            html:'Second view!'
+                        })
+                    }
+                }
+            },
+            items:[{
+                xtype:'image',
+                id:'dupont',
+                src:'resources/images/dupont.jpg',
+                flex: 1
+            },{
+                xtype:'image',
+                src:'resources/images/Data_analysis.jpg',
+                flex: 1
+            },{
+                xtype:'image',
+                src:'resources/images/IDEA.jpg',
+                flex: 1
+            }]
         }]
     }
-
 });
